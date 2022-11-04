57920c9572d3d47d91236225011b89c8143b3883
Muhammad Mubashir Mughal <c0861455@mylambton.ca>
2022-11-04 19:16:13 -0400

    Initial commit

---
+++ MuhammadMubashir_c0861455_LabTest/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  MuhammadMubashir_c0861455_LabTest
+//
+//  Created by MacStudent on 2022-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TimerLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var timer:Timer = Timer()
+    var count:Int = 0
+    var timmerCounting:Bool = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startStopButton.setTitleColor(UIColor.green, for: .normal)
+    }
+
+    @IBAction func resetTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Reset Timer?", message: "Are you sure you would like to reset the timer?", preferredStyle: .alert)
+       // alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: {(_) in
+            //do nothing
+     //   } ))
+        alert.addAction(UIAlertAction(title: "YES", style: .cancel, handler: {(_) in
+            self.count = 0
+            self.timer.invalidate()
+            self.TimerLabel.text = self.makeTimeString(hours: 0, minutes: 0, seconds: 0)
+            self.startStopButton.setTitle("START", for: .normal)
+            self.startStopButton.setTitleColor(UIColor.green, for: .normal)
+        } ))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func startstopTapped(_ sender: Any) {
+        if (timmerCounting) {
+            timmerCounting = false
+            timer.invalidate()
+            startStopButton.setTitle("START", for: .normal)
+            startStopButton.setTitleColor(UIColor.green, for: .normal)
+        } else {
+            timmerCounting = true
+            startStopButton.setTitle("STOP", for: .normal)
+            startStopButton.setTitleColor(UIColor.red, for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func timerCounter() -> Void {
+        count = count + 1
+        let time = secondsToHoursMinutesSeconds(seconds: count)
+        let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+        TimerLabel.text = timeString
+    }
+    func secondsToHoursMinutesSeconds(seconds: Int) -> (Int,Int,Int) {
+        
+        return ((seconds / 3600),((seconds % 3600) / 60),((seconds % 3600) % 60))
+    }
+    func makeTimeString(hours: Int, minutes: Int, seconds: Int) -> String {
+        var timeString = ""
+        timeString += String(format: "%02d", hours)
+        timeString += "  :  "
+        timeString += String(format: "%02d", minutes)
+        timeString += "  :  "
+        timeString += String(format: "%02d", seconds)
+        return timeString
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
